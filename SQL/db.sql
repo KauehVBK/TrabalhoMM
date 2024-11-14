b0e9ad187c2b5f037028117d26a3c727d1c7c403
Kauêh vitali Bento <67390551+KauehVBK@users.noreply.github.com>
2024-11-13 21:14:26 -0300

    Add files via upload

--- SQL/db.sql
+++ SQL/db.sql
@@ -48,3 +48,39 @@ insert into pokemon(nome, type) values
 ('butterfree','Voador'),
 ('butterfree','Voador'),
 ('fuecoco', 'Fogo');
+
+-- VIEW PARA STATUS DE SE É DUPLICADO E PQ FOI DELETADO
+
+CREATE VIEW vw_pokemon_status AS
+SELECT 
+    p.id,
+    p.nome,
+    p.type,
+    'Átivo' AS status,
+    NULL AS razao_remocao
+FROM 
+    pokemon p
+WHERE p.id NOT IN (SELECT id FROM pokemon_deletados)
+
+UNION ALL
+
+SELECT 
+    pd.id,
+    pd.nome,
+    pd.type,
+    'Removido' AS status,
+    'Duplicado' AS razao_remocao
+FROM 
+    pokemon_deletados pd;
+
+-- VIEW PARA CALCULO DE REPETIDOS
+
+CREATE VIEW vw_pokemon_calculo_repetidos AS
+SELECT 
+    p.type AS tipo,
+    (COUNT(p.id) + (SELECT COUNT(id) FROM pokemon_deletados d WHERE d.type = p.type)) AS valor_total,
+    (SELECT COUNT(id) FROM pokemon_deletados d WHERE d.type = p.type) AS count_deletados,
+    (COUNT(p.id) + (SELECT COUNT(id) FROM pokemon_deletados d WHERE d.type = p.type)) - (SELECT COUNT(id) FROM pokemon_deletados d WHERE d.type = p.type) AS resultado_final
+FROM 
+    pokemon p
+GROUP BY p.type;
